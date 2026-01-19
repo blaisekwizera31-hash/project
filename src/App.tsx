96dc9aaea6ad7b1501ba7655ca7cbf2f635c67b7
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-19 10:36:10 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import CitizenDashboard from "./pages/CitizenDashboard";
 import LawyerDashboard from "./pages/LawyerDashboard";
+import Appointments from "./pages/Appointments";
+import LegalResources from "./pages/LegalResources";
+import Settings from "./pages/Settings";
 import JudgeDashboard from "./pages/JudgeDashboard";
 import CourtClerkDashboard from "./pages/ClerkDashboard";
 import NotFound from "./pages/NotFound";
@@ -24,12 +27,15 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Index />} />
+           <Route path="/appointments" element={<Appointments />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<CitizenDashboard />} />
           <Route path="/lawyer-dashboard" element={<LawyerDashboard />} />
           <Route path="/judge-dashboard" element={<JudgeDashboard />} />
           <Route path="/clerk-dashboard" element={<CourtClerkDashboard />} />
           <Route path="*" element={<NotFound />} />
+           <Route path="/legal-resources" element={<LegalResources />} />
+          <Route path="/settings" element={<Settings />} />
           <Route path="/dashboard" element={<CitizenDashboard />} />
           <Route path="/find-lawyer" element={<FindLawyer />} />
           <Route path="/submit-case" element={<SubmitCase />} />
